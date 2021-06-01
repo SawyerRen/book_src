e3bf29e5bc2748a567ef75c744fcea1fa87d3ee0
rensiyu.1121 <rensiyu.1121@bytedance.com>
2021-06-01 10:16:17 +0800

    ch03 decorator

---
+++ headfirstDesignPattern/src/ch03/Beverage.java
@@ -0,0 +1,11 @@
+package ch03;
+
+public abstract class Beverage {
+    String description = "unknown beverage";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+}
---
+++ headfirstDesignPattern/src/ch03/HouseBlend.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class HouseBlend extends Beverage {
+    public HouseBlend() {
+        description = "HouseBlend";
+    }
+
+    @Override
+    public double cost() {
+        return 0.89;
+    }
+}
